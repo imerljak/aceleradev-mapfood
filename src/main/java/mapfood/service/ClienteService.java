3f5fac4e0b67bef42bf09ebaed82bea49dd05369
Israel Vargas Merljak <imerljak@gmail.com>
2019-01-31 18:55:17 -0200

    ajustando Services p/ retornar os DTOs

--- src/main/java/mapfood/service/ClienteService.java
+++ src/main/java/mapfood/service/ClienteService.java
@@ -1,14 +1,24 @@
 package mapfood.service;
 
-import mapfood.model.jpa.Cliente;
+import mapfood.model.dto.ClienteDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ClienteService {
 
-    List<Cliente> buscaTodos();
+    /**
+     * Retorna uma lista com todos clientes registrados na base de dados
+     *
+     * @return lista de clientes
+     */
+    List<ClienteDTO> buscaTodos();
 
-    Optional<Cliente> buscaPorId();
+    /**
+     * Busca e retorna um cliente pelo seu Id
+     *
+     * @return opcional de cliente
+     */
+    Optional<ClienteDTO> buscaPorId(Long id);
 
 }
--- src/main/java/mapfood/service/MotoboyService.java
+++ src/main/java/mapfood/service/MotoboyService.java
@@ -1,6 +1,6 @@
 package mapfood.service;
 
-import mapfood.model.jpa.Motoboy;
+import mapfood.model.dto.MotoboyDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,25 +9,27 @@ public interface MotoboyService {
 
     /**
      * Retorna uma lista contendo todos os Motoboys registrados na base de dados.
+     *
      * @return lista de motoboys
      */
-    List<Motoboy> buscaTodos();
+    List<MotoboyDTO> buscaTodos();
 
     /**
      * Retorna uma lista contendo todos os motoboys por proximidade detro de do raioEmKm
      * ordenados por distancia crescente.
      *
-     * @param latitude latitude inicial
+     * @param latitude  latitude inicial
      * @param longitude longitude inicial
-     * @param raioEmKm raio em kms para considerar a pesquisa.
+     * @param raioEmKm  raio em kms para considerar a pesquisa.
      * @return lista de motoboys ordenada pela distancia relativa a lat/long informada.
      */
-    List<Motoboy> buscaPorAproximacao(Double latitude, Double longitude, Double raioEmKm);
+    List<MotoboyDTO> buscaPorAproximacao(Double latitude, Double longitude, Double raioEmKm);
 
     /**
-     * Busca um {@link Motoboy} pelo seu id e retorna um {@link Optional} contendo o motoboy ou não, caso não exista.
+     * Busca um motoboy pelo seu id e retorna um {@link Optional} contendo o motoboy ou não, caso não exista.
+     *
      * @return motoboy, ou não :)
      */
-    Optional<Motoboy> buscaPorId(Long id);
+    Optional<MotoboyDTO> buscaPorId(Long id);
 
 }
