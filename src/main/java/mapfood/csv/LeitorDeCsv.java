3c16b3404bedbe785243259f1dfb505b2500db69
Israel Vargas Merljak <imerljak@gmail.com>
2019-01-31 18:56:14 -0200

    helper p/ leitura dos csv's

---
+++ src/main/java/mapfood/csv/LeitorDeCsv.java
@@ -0,0 +1,44 @@
+package mapfood.csv;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import mapfood.data.ImportacaoDeDadosCsvConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+public class LeitorDeCsv {
+
+    private Logger logger = LoggerFactory.getLogger(ImportacaoDeDadosCsvConfig.class);
+
+
+    public <T> List<T> lerDados(Class<T> tClass, String filename) {
+        try {
+
+            logger.debug("Iniciando parsing de CSV.");
+
+            CsvSchema schema = CsvSchema.emptySchema().withHeader();
+            CsvMapper mapper = new CsvMapper();
+
+            File file = new ClassPathResource(filename).getFile();
+
+            logger.debug("Arquivo aberto: {}", file);
+
+            MappingIterator<T> iterator = mapper.readerFor(tClass).with(schema).readValues(file);
+
+            return iterator.readAll();
+        } catch (Exception e) {
+            //TODO: Log error.
+
+            logger.error(e.getMessage(), e);
+
+            return Collections.emptyList();
+        }
+    }
+
+}
