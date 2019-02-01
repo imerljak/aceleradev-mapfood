28941bd7ac7cdd7f5b8e0b39faa68373a5645801
diovanemendes <diovane.m.mendes@gmail.com>
2019-02-01 00:36:27 -0200

    Testes: leitura csv e problemas em testar persistencia

---
+++ src/test/java/mapfood/csv/LeitorDeCsvTest.java
@@ -0,0 +1,24 @@
+package mapfood.csv;
+
+import mapfood.model.dto.ClienteDTO;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LeitorDeCsvTest {
+
+    @Test
+    public void deveRetornarFalseSeCaminhoArquivoEstiverCerto(){
+        LeitorDeCsv leitor = new LeitorDeCsv();
+
+        assertFalse(leitor.lerDados(ClienteDTO.class, "/csv/clientes.csv").isEmpty());
+    }
+
+    @Test
+    public void deveRetornarTrueSeCaminhoArquivoEstiverErrado(){
+        LeitorDeCsv leitor = new LeitorDeCsv();
+
+        assertTrue(leitor.lerDados(ClienteDTO.class, "/resources/clientes.csv").isEmpty());
+    }
+}
