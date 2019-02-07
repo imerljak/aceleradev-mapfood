d100f3dbdb31b94f3c2884d644facd9d3df45740
Israel Vargas Merljak <imerljak@gmail.com>
2019-02-06 23:35:18 -0200

    fix EstabelecimentoService

--- src/main/java/mapfood/service/EstabelecimentoService.java
+++ src/main/java/mapfood/service/EstabelecimentoService.java
@@ -1,5 +1,20 @@
 package mapfood.service;
 
+import mapfood.model.mongodb.Estabelecimento;
+
+import java.util.Optional;
+
 public interface EstabelecimentoService {
 
+    /**
+     * Retorna estabelecimento pelo id
+     * @param id
+     * @return estabelecimento
+     */
+    Optional<Estabelecimento> findById(String id);
+
+
+    /*
+     * TODO: método para retornar relalório de entregas do estabelecimento.
+     */
 }
