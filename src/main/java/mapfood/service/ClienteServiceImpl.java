c553f69df2e066da76d6eb3e0eedc3d40407f516
Israel Vargas Merljak <imerljak@gmail.com>
2019-02-06 23:32:19 -0200

    fix clienteService

--- src/main/java/mapfood/service/ClienteServiceImpl.java
+++ src/main/java/mapfood/service/ClienteServiceImpl.java
@@ -1,36 +1,36 @@
 package mapfood.service;
 
+import mapfood.factory.ClienteFactory;
+import mapfood.model.dto.ClienteDTO;
+import mapfood.repository.sql.ClienteRepository;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoOperations;
-
-import mapfood.factory.ClienteFactory;
-import mapfood.model.dto.ClienteDTO;
-import mapfood.model.jpa.Cliente;
-import mapfood.repository.sql.ClienteRepository;
-
-
+@Service
 public class ClienteServiceImpl implements ClienteService {
-	
 
-	@Autowired
-	private ClienteRepository cliente;
-	
-	@Override
-	public List<ClienteDTO> buscaTodos() {
-		return StreamSupport.stream(cliente.findAll().spliterator(), false).map(ClienteFactory::getInstance).collect(Collectors.toList());	
-	}
 
-	@Override
-	public Optional<ClienteDTO> buscaPorId(Long id) {
-		return null; //ClienteFactory.getInstance(cliente.findById(id);
-		
-	}
+    private final ClienteRepository repository;
 
-	
+    public ClienteServiceImpl(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<ClienteDTO> buscaTodos() {
+        return repository.findAll()
+                .stream()
+                .map(ClienteFactory::getInstance)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<ClienteDTO> buscaPorId(Long id) {
+        return repository.findById(id).map(ClienteFactory::getInstance);
+    }
+
 
 }
