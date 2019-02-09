3bf643127808162fd2a01d17abfa45453297350d
Sarah <sarah.lacerda@acad.pucrs.br>
2019-02-09 11:39:57 -0200

    Adicionando relatorio de entregas para o estabelecimento

--- src/main/java/mapfood/service/DadosEntregaService.java
+++ src/main/java/mapfood/service/DadosEntregaService.java
@@ -1,6 +1,12 @@
 package mapfood.service;
 
+import mapfood.model.dto.ClienteDTO;
+import mapfood.model.mongodb.DadosEntrega;
+import java.util.List;
+
 public interface DadosEntregaService {
 
-	
+    List<DadosEntrega> buscaPorIdAndAfterData(String id, int days);
+
+
 }
